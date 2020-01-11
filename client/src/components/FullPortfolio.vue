595f92d401e1104ff4e3447a6b713fbaafa5ebb7
Rebecca Pae <r.pae92@gmail.com>
2020-01-11 14:41:34 -0800

    styling for the full portfolio

--- client/src/components/FullPortfolio.vue
+++ client/src/components/FullPortfolio.vue
@@ -4,42 +4,116 @@
     <br>
     <div class="card">
       <h5 class="card-header">moyo</h5>
-      <div class="card-body">
-        <h5 class="card-title">A financial education app centered around stories and mini-games</h5>
-        <p class="card-text">An app reimagining the finance experience</p>
-        <button type="button" class="btn" id="special-button">
-        <router-link to="/moyo_details">Learn More ➜</router-link>
-        </button>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="@/assets/SVG/neos-moyo-website.svg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A financial education app centered around stories and mini-games</h5>
+          <p class="card-text">An app reimagining the finance experience</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/moyo_details">Learn More ➜</router-link>
+          </button>
+        </div>
       </div>
+      <br>
     </div>
     <br>
     <div class="card">
       <h5 class="card-header">The mining game</h5>
-      <div class="card-body">
-        <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
-        <p class="card-text">A node program that runs in your terminal.</p>
-        <button type="button" class="btn" id="special-button">
-        <router-link to="/mining-game">Learn More ➜</router-link>
-        </button>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="..//assets/SVG/profile_image.jpg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
+          <p class="card-text">A node program that runs in your terminal.</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/mining-game">Learn More ➜</router-link>
+          </button>
+        </div>
       </div>
+      <br>
+    </div>
+    <br>
+        <div class="card">
+      <h5 class="card-header">Project 3</h5>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="..//assets/SVG/profile_image.jpg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
+          <p class="card-text">A node program that runs in your terminal.</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/">Learn More ➜</router-link>
+          </button>
+        </div>
+      </div>
+      <br>
     </div>
     <br>
     <div class="card">
-      <h5 class="card-header">Featured</h5>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <router-link class="btn btn-primary" to="/moyo_details">Learn more</router-link>
+      <h5 class="card-header">Project 4</h5>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="..//assets/SVG/profile_image.jpg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
+          <p class="card-text">A node program that runs in your terminal.</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/">Learn More ➜</router-link>
+          </button>
+        </div>
       </div>
+      <br>
     </div>
     <br>
     <div class="card">
-      <h5 class="card-header">Featured</h5>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">See Project</a>
+      <h5 class="card-header">Project 5</h5>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="..//assets/SVG/profile_image.jpg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
+          <p class="card-text">A node program that runs in your terminal.</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/">Learn More ➜</router-link>
+          </button>
+        </div>
       </div>
+      <br>
+    </div>
+    <br>
+    <div class="card">
+      <h5 class="card-header">Project 6</h5>
+      <div class="row">
+        <div class="col-sm-6">
+          <img src="..//assets/SVG/profile_image.jpg" class="card-img-top">
+        </div>
+        <div class="col-sm-6">
+          <div class="card-body">
+          </div>
+          <h5 class="card-title">A simplified game that uses cryptocurrency prices and cryptographic puzzles to mine fictious coins</h5>
+          <p class="card-text">A node program that runs in your terminal.</p>
+          <button type="button" class="btn" id="special-button">
+          <router-link to="/">Learn More ➜</router-link>
+          </button>
+        </div>
+      </div>
+      <br>
     </div>
     <br>
   </div>
@@ -62,6 +136,17 @@ h1 {
   background-color: #8ee4af;
   color: #05386b;
   display: block;
+  margin: auto;
+}
+
+.card-img-top {
+  width: 40%;
+  height: auto;
+  margin: auto;
+  margin-top: 3%;
+}
+
+.col-sm-6 {
   margin: auto;
 }
 
