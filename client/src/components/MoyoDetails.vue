1de2b3556a285cd4df725a0aca4086575a38c318
Tian3401 <matt.chen3401@yahoo.com>
2020-01-11 18:37:42 -0800

    incorporate  moyo project details and images

--- client/src/components/MoyoDetails.vue
+++ client/src/components/MoyoDetails.vue
@@ -5,14 +5,12 @@
     <button type="button" class="btn" id="special-button">
     <a href="https://themoyoproject.com" target="_blank">Visit Official Website ➜</a>
     </button>
-    <br>
-    <h1>The Purpose Project</h1>
     <div class="row mt-5 bg-white mb-4">
       <div
         class="col-sm-6"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/barista_creative_notes.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'left' }"
+        :style="{'min-height': '500px','background-image': 'url(\'' + require('@/assets/img/moyoAvatarsImg.png') + '\')', 'background-size': 'initial', 'background-repeat': 'no-repeat', 'background-position': 'center'}"
       ></div>
-      <div class="col-12 col-lg-6 p-5 d-flex">
+      <div class="col-12 col-sm-6 p-5 d-flex">
         <p class="px-5 d-flex align-self-center">
           The motivation behind moyo is to reimagine the financial experience for the end users. Using stories and gameplay, moyo presents finance in a simplified and applicable way. This approach leverages the intrigue of a story in explaining concepts such as time value of money and compound interest and the challenge of games to help users gain exposure to investing, budgetting, and other healthy financial habits. 
         </p>
@@ -26,26 +24,24 @@
       </div>
       <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/SVG/neos-moyo-website.svg') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat' }"
+        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/having_coffee.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat' }"
       ></div>
     </div>
     <div class="row bg-white mb-4">
       <div class="col-12 mx-auto p-5 d-flex">
         <p
           class="px-5 d-flex align-self-center emphasis-text"
-        >In other words, goal- and scenario-based risk-free trading with tightly controlled (and more importantly, deliberately limited) unexpected variables will allow learners to walk away educated and empowered. This risklessness is essential in an ecosystem of industry-driven financial education which exclusively serves to funnel users into financial products (such as brokerage accounts) which necessitate real-world risk.</p>
+        >Finance is a part of everyone's life and impact the decisions we make. moyo can help take the first step in a person's financial journey.</p>
       </div>
     </div>
     <div class="row bg-white mb-4">
       <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/having_coffee.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom center' }"
+        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/technology.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'center' }"
       ></div>
       <div class="col-12 col-sm-6 p-5 d-flex">
         <h1 class="p-5 d-flex align-self-center">
-          Key Insights/Challenges
-        <!-- <h2>Key Insights</h2>
-        <h2>The website</h2> -->
+          Web and System Architecture
         </h1>
       </div>
     </div>
@@ -53,54 +49,35 @@
     <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
         <p class="px-5 d-flex align-self-center">
-          The goal of any real-life interpersonal relationship is to connect; to exchange anecdotes, ideas, likes and dislikes, hardships, and empathy. This rule doesn’t change in business, nor in education. As human beings, we’re hardwired for stories and dialogues.
+          During this project, I was able to touch many different aspects from web devlopement to application development to system design. 
           <br>
-          <br>In an era of MOOCs (massively open online courses) and shifting paradigms in formal education, new frontiers in accessible adult learning and professional development are constantly being pushed. While the MOOC format has largely democratized access to advanced learning resources, they often fail to resonate with students and audiences.
+          <br>
+          The web platform uses AWS to service its backend, where a cluster of instances are deployed for redundancy. An elastic load balancer is used for load management. The data layer encorporates a noSQL database and third party APIs.
         </p>
       </div>
       <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/engineer_creative_notes.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'right' }"
+        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/systemArchitecture.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'right' }"
       ></div>
     </div>
     <div class="row bg-white mb-4 pt-5 p-lg-0">
-      <div
-        class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/girl_creative_notes.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom right' }"
-      ></div>
-      <div class="col-12 col-lg-6 p-5 d-flex">
-        <h1 class="p-5 d-flex align-self-center">UI and UX development</h1>
-      </div>
+        <h1 class="p-5 d-flex align-self-center">Designing the App</h1>
     </div>
     <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
           <p class="px-5 d-flex align-self-center">
-          Those which succeed (based purely on the metric of content provided, not considerations such as cost, software, etc.), tend to do so on the merits of a distinct and recognizable content provider--a unique face and voice serving as the locus of authority and credibility.
-          <br>
-          <br>For third-party content aggregate MOOC providers, such as Udemy or Coursera, there is no unifying story across learning modules. Content providers are responsible for converting and retaining their users and followers, while the platforms are more akin to brokers, bringing user and provider together.
-          <br>
-          <br>Moyo needs to be more. While Moyo will thrive on a robust content-provider ecosystem, that content must never be so platform-independent so as to break the user immersion of their story and dialogue with the platform itself.
+          The mobile platform seemed to be the most intuitive for what the projected sought to accomplish. 
+          The user interface is inspired by Twitter in regards to the social aspects of the app, but the app navigation is similar to that of Headspace's.The app enables users to explore the world of finance together through interesting stories on their feed and co-op gameplay of the mini-games in the sandbox section. 
         </p>
-
       </div>
-      <div
-        class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/builder_creative_notes.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom right' }"
-      ></div>
-      <div class="col-12 mx-auto p-5 d-flex">
-      <p class="px-5 d-flex align-self-center emphasis-text">
-      AirBnB succeeds on telling the stories of experiences, shaped by the places its users visit. Goodlife Fitness succeeds by telling the stories of everyday fitness heroes, rather than those of hyper-sculpted stock-photo models. Warby Parker succeeds by telling the story of every individual pair of glasses, socially-consciously manufactured using plant-based materials, polished by hand, and often contributed en masse to charitable partners throughout the globe.
-      <br>
-      <br>Moyo doesn’t have the physical or locational product stories of these brands (although some day, it might). What it does have is the story of its target users and the story of the problem it seeks to solve.
-      <br>
-      <br>How does a pre-launch digital product aimed at aggregating third-party content to directly benefit a largely financial-literacy-averse millennial generation?
-    </p>
+      <div class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0">
+      <img src="@/assets/img/loginScreenApp.png" alt="">
       </div>
     </div>
     <div class="row bg-white mb-4">
       <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/having_coffee.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom center' }"
+        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/appUserScreen.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom center' }"
       ></div>
       <h1 class="p-5 d-flex align-self-center">App Development</h1>
     </div>
@@ -108,33 +85,28 @@
       <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
           <p class="px-5 d-flex align-self-center">
-          Using unique, original characters, with their own stories, their own journeys, and their own evolutions.
+         React Native is language of choice for this app. The benefits of being able to deploy on both Android and iOS outweigh the potential benefits programming for a specific operating system. 
           <br>
-          <br>More than brand ambassadors, Moyo’s character avatars will serve as loci of appeal and empathy, through which they can connect with the brand.
           <br>
-          <br>The most important consideration in achieving this will be toeing the balance between character appeal and authenticity. It’s no secret that these characters and their stories are fictional and heavily stylized, but their story and their experiences will ring true to users for fostering those empathetic connections, while sustaining the appropriate degree of suspension of disbelief to facilitate users’ willful and enthusiastic buy-in.
+          The app is geared towards a Millennial audience, but I am confident that it will be appealing to a diverse range of users.
         </p>
       </div>
         <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0 py-5"
-        :style="{ 'min-height': '400px', 'background-image': 'url(\'' + require('@/assets/img/welcome.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'center right' }"
+        :style="{ 'min-height': '600px', 'background-image': 'url(\'' + require('@/assets/img/millennials.png') + '\')', 'background-size': 'cover', 'background-repeat': 'no-repeat', 'background-position': 'bottom' }"
       ></div>
     </div>
 
-   <div class="row bg-white mb-4">
+   <!-- <div class="row bg-white mb-4">
       <div class="col-12 mx-auto p-5 d-flex">
           <p class="px-5 d-flex align-self-center">
-          Moyo’s focus is opening access to quality financial education and resources to everyone. As a result, inclusion and diversity are key values, represented across the Moyo brand. This includes diversity of all kinds, not just financial or socioeconomic.
-          <br>
-          <br>Our character product ambassadors, just like our future users, are representative of diversity across the spectrum. They embody the guidelines which set the tone for Moyo’s identity. They exist to do more than simply delight or even to inform our users and audience. They exist to be related to, to be welcoming, to be paragons of positivity who celebrate differences and the break-down of the traditional archetypes representing financial success.
+          
         </p>
           <p class="p-5 d-flex align-self-center">
-          Our characters come from all backgrounds, all ages, all races, all creeds; they carry their own goals and dreams; they bring with them their own fears; they reflect all of our users, anybody aiming to achieve financial independence and security. They reflect everyone, full stop.
-          <br>
-          <br>Moyo is for everyone, full stop.
+          
         </p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -150,6 +122,7 @@
 
 h1 {
   display: block;
+  margin: auto;
 }
 
 .title {
@@ -165,6 +138,12 @@ h1 {
   background-color: #8ee4af;
   color: #05386b;
   display: block;
+  margin: auto;
+}
+
+img {
+  width: 60%;
+  height: auto;
   margin: auto;
 }
 </style>
