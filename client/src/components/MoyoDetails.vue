0f29443888dbb9e880ae25eaa6df9120144d5940
Tian3401 <matt.chen3401@yahoo.com>
2020-01-13 18:10:02 -0800

    update copy

--- client/src/components/MoyoDetails.vue
+++ client/src/components/MoyoDetails.vue
@@ -12,7 +12,10 @@
       ></div>
       <div class="col-12 col-sm-6 p-5 d-flex">
         <p class="px-5 d-flex align-self-center">
-          The motivation behind moyo is to reimagine the financial experience for the end users. Using stories and gameplay, moyo presents finance in a simplified and applicable way. This approach leverages the intrigue of a story in explaining concepts such as time value of money and compound interest and the challenge of games to help users gain exposure to investing, budgetting, and other healthy financial habits. 
+          My involvement with moyo is what lead me to programming. It's been at the center of my life and career for the past few years now and I am so grateful that I've been able to work on such a ambitious and fun project with other great people. I've grown a lot as a developer on this project.
+          <br>
+          <br>
+          The motivation behind moyo is to reimagine the financial experience for the vast majority of us. Using stories and gameplay, moyo presents finance in a simplified and relatable way. This approach leverages the intrigue of a story in explaining financial and money concepts such as time value of money and compound interest and the challenge of games to help the normal person gain exposure to investing, budgetting, and other healthy financial habits. 
         </p>
       </div>
     </div>
@@ -20,7 +23,7 @@
       <div class="col-12 col-lg-6 p-5 d-flex">
         <p
           class="px-5 d-flex align-self-center"
-        >To help with the stories and games, moyo utilizes avatars. These characters are meant to help to recharacterize finance as a whole, making the subject matter more approachable and less intimidating for end user. Each avatar has his or her own story, which enables a deeper connection between users and these characters.</p>
+        >To help with the stories and games, moyo utilizes avatars. These characters are meant to help to recharacterize finance as a whole, making the subject matter more approachable and less intimidating for the user. Each avatar has his or her own story, allowing a deeper connection with users.</p>
       </div>
       <div
         class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0"
@@ -31,7 +34,7 @@
       <div class="col-12 mx-auto p-5 d-flex">
         <p
           class="px-5 d-flex align-self-center emphasis-text"
-        >Finance is a part of everyone's life and impact the decisions we make. moyo can help a person take the first step in a person's financial journey.</p>
+        > In my opinion, finance is a part of everyone's life and impact the decisions we make. I wanted to create and design moyo in a very specific way that helps introduce finance and money to the average person.</p>
       </div>
     </div>
     <div class="row bg-white mb-4">
@@ -41,7 +44,7 @@
       ></div>
       <div class="col-12 col-sm-6 p-5 d-flex">
         <h1 class="p-5 d-flex align-self-center">
-          Web and System Architecture
+          Web Development and System Architecture
         </h1>
       </div>
     </div>
@@ -49,10 +52,10 @@
     <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
         <p class="px-5 d-flex align-self-center">
-          During this project, I was able to touch many different aspects from web devlopement to application development to system design. 
+          While working on such a big project, exposure to both frontend and backend development was inevitable. Because I started this project on my own, I've had to do a lot of the initial programming myself. This included setting up the website as well as designing and implementing the system architecture on my own. Scalability is still an aspect of developement I am still working to improve, but I'm improving everyday. 
           <br>
           <br>
-          The web platform uses AWS to service its backend, where a cluster of instances are deployed for redundancy. An elastic load balancer is used for load management. The data layer encorporates a noSQL database and third party APIs.
+          To give a little more detail on the the system architecture, the website is hosted on dedicated web servers and uses AWS to service its backend, where a cluster of instances are deployed for redundancy. An elastic load balancer is used for load management. The data layer encorporates a noSQL database and third party APIs.
         </p>
       </div>
       <div
@@ -66,8 +69,10 @@
     <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
           <p class="px-5 d-flex align-self-center">
-          The mobile platform seemed to be the most intuitive for what the projected sought to accomplish. 
-          The user interface is inspired by Twitter in regards to the social aspects of the app, but the app navigation is similar to that of Headspace's.The app enables users to explore the world of finance together through interesting stories on their feed and co-op gameplay of the mini-games in the sandbox section. 
+            The mobile platform seemed to be the most intuitive for what the projected sought to accomplish, an insight that contributing members have helped me realize. You can deduce that this revelation meant I needed to understand the basics of UI and UX. Luckily, I haven't had to tackle this on my own. 
+          <br>
+          <br>
+            The user interface is inspired by Twitter in regards to the social aspects of the app, but the app navigation is similar to that of Headspace's.The app enables users to explore the world of finance together through interesting stories on their feed and co-op gameplay of the mini-games in the sandbox section. 
         </p>
       </div>
       <div class="col-9 mx-auto mb-5 mb-lg-0 col-lg-6 p-0">
@@ -85,10 +90,10 @@
       <div class="row bg-white mb-4">
       <div class="col-12 col-lg-6 p-5 d-flex">
           <p class="px-5 d-flex align-self-center">
-         React Native is language of choice for this app. The benefits of being able to deploy on both Android and iOS outweigh the potential benefits programming for a specific operating system. 
+            The team and I have selected React Native as the language of choice. The benefits of being able to deploy on both Android and iOS outweigh the potential benefits of programming for a specific operating system. 
           <br>
           <br>
-          The app is geared towards a Millennial audience, but I am confident that it will be appealing to a diverse range of users.
+            The app is geared towards a Millennial audience, but can be used by anyone. There is still quite a bit of work still to be done before the app is released, but it will be worth it. 
         </p>
       </div>
         <div
